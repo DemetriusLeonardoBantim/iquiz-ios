c27502239e0bbb2cf495e728bfb7ceb67a1a2566
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-04-23 21:55:33 -0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iquiz
+//
+//  Created by Demetrius  on 23/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
